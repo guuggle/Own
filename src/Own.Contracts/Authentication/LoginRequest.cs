159a804cb6200dd43d4b9f621737e121af9f1911
guuggle <kandejianer@gmail.com>
2023-05-01 12:13:23 +0800

    test

--- src/Own.Contracts/Authentication/LoginRequest.cs
+++ src/Own.Contracts/Authentication/LoginRequest.cs
@@ -4,5 +4,6 @@ namespace Own.Contracts.Authentication
     {
         public string Email { get; set; }
         public string Password { get; set; }
+        public int MyProperty { get; set; }
     }
 }
